1ffc72103a72fbde4c6686278c480c0ac92bd9a9
godlevsk1y <godlevskiy.is.working@gmail.com>
2026-07-01 13:50:13 +0300

    feat: add db migrations

---
+++ backend/DirectoryService/src/DirectoryService.Infrastructure.Postgres/Migrations/20260701103147_ChangeDepartmentLocationIsPrimaryName.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DirectoryService.Infrastructure.Postgres.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeDepartmentLocationIsPrimaryName : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "IsPrimary",
+                table: "department_locations",
+                newName: "is_primary");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "is_primary",
+                table: "department_locations",
+                newName: "IsPrimary");
+        }
+    }
+}
---
+++ backend/DirectoryService/src/DirectoryService.Infrastructure.Postgres/Migrations/20260701104003_ChangeLocationNameName.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DirectoryService.Infrastructure.Postgres.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeLocationNameName : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                table: "locations",
+                newName: "name");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "name",
+                table: "locations",
+                type: "character varying(100)",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "name",
+                table: "locations",
+                newName: "Name");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "locations",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(100)",
+                oldMaxLength: 100);
+        }
+    }
+}
