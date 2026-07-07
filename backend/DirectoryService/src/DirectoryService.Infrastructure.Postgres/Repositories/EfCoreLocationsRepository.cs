fc587fcec482b0c2f2e703ad96c2b39b2dc6275e
godlevsk1y <godlevskiy.is.working@gmail.com>
2026-07-08 00:19:28 +0300

    feat: add EfCoreLocationsRepository

---
+++ backend/DirectoryService/src/DirectoryService.Infrastructure.Postgres/Repositories/EfCoreLocationsRepository.cs
@@ -0,0 +1,34 @@
+using DirectoryService.Core.Locations;
+using DirectoryService.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DirectoryService.Infrastructure.Postgres.Repositories;
+
+public class EfCoreLocationsRepository : ILocationsRepository
+{
+    private readonly DirectoryServiceDbContext _context;
+
+    public EfCoreLocationsRepository(DirectoryServiceDbContext context)
+    {
+        _context = context;
+    }
+    
+    public async Task<Guid> AddAsync(Location location, CancellationToken cancellationToken)
+    {
+        await _context.Locations.AddAsync(location, cancellationToken);
+        
+        await _context.SaveChangesAsync(cancellationToken);
+        
+        return location.Id;
+    }
+
+    public async Task<Location?> GetByIdAsync(Guid id, CancellationToken cancellationToken)
+    {
+        return await _context.Locations.FirstOrDefaultAsync(l => l.Id == id, cancellationToken);
+    }
+
+    public async Task<Location?> GetByNameAsync(string name, CancellationToken cancellationToken)
+    {
+        return await _context.Locations.FirstOrDefaultAsync(l => l.Name == name, cancellationToken);
+    }
+}
