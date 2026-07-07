5b9a104cd7b7487e8159c8ddc851b118e610122e
godlevsk1y <godlevskiy.is.working@gmail.com>
2026-07-07 19:52:52 +0300

    feat: add db sets to db context

--- backend/DirectoryService/src/DirectoryService.Infrastructure.Postgres/DirectoryServiceDbContext.cs
+++ backend/DirectoryService/src/DirectoryService.Infrastructure.Postgres/DirectoryServiceDbContext.cs
@@ -1,9 +1,14 @@
+using DirectoryService.Domain.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace DirectoryService.Infrastructure.Postgres;
 
 public sealed class DirectoryServiceDbContext : DbContext
 {
+    public DbSet<Department> Departments { get; set; }
+    public DbSet<Location> Locations { get; set; }
+    public DbSet<Position> Positions { get; set; }
+    
     public DirectoryServiceDbContext(DbContextOptions<DirectoryServiceDbContext> options)
         : base(options) { }
 
